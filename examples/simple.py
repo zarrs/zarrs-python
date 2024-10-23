4c87da3b75d4647300a534ebd635d158df9f4a06
Lachlan Deakin <ljdgit@gmail.com>
2024-10-24 09:54:13 +1100

    (fix): minimum working codec pipeline
    - Add internal `get_chunk_representation`, `retrieve_chunk_bytes`, and `store_chunk_bytes`
    - Separate `retrieve_chunk` and `retrieve_chunk_subset`
    - Separate `store_chunk` and `store_chunk_subset`
    - Add assertions to simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -12,20 +12,34 @@ chunks = (2,2)
 shape = (4,4)
 
 tmp = tempfile.TemporaryDirectory()
-arr = zarr.zeros(shape, store=LocalStore(root=tmp.name, mode='w'), chunks=chunks, dtype=np.uint8, codecs=[zarr.codecs.BytesCodec(), zarr.codecs.BloscCodec()])
+arr = zarr.zeros(shape, store=LocalStore(root=tmp.name, mode='w'), chunks=chunks, dtype=np.int16, codecs=[zarr.codecs.BytesCodec(), zarr.codecs.BloscCodec()])
 
 # arr = zarr.zeros(shape, store=MemoryStore(mode='w'), chunks=chunks, dtype=np.uint8, codecs=[zarr.codecs.BytesCodec(), zarr.codecs.BloscCodec()])
 
+# Check fill value decoding
 print(arr[:])
+assert np.all(arr[:] == 0)
+
+# Store a constant
 # arr[:] = 42 # This is actually a special case that needs to be handled
+
+# Store a numpy array
 arr[:] = np.arange(16).reshape(4,4)
 print(arr[:])
+assert np.all(arr[:] == np.arange(16).reshape(4,4))
+
+# Decode a chunk
+print(arr[0:2,0:2])
+assert np.all(arr[0:2,0:2] == np.array([[0, 1], [4, 5]]))
 
 # Partial decoding
-# TODO: Works but decodes each chunk entirely
 print(arr[0:1,1:2])
+assert np.all(arr[0:1,1:2] == np.array([[1]]))
 print(arr[1:3,1:3])
+assert np.all(arr[1:3,1:3] == np.array([[5, 6], [9, 10]]))
 
 # Partial encoding
 # TODO
 arr[1:3,1:3] = np.array([[-1, -2], [-3, -4]])
+print(arr[:])
+assert np.all(arr[:] == np.array([[0, 1, 2, 3], [4, -1, -2, 7], [8, -3, -4, 11], [12, 13, 14, 15]]))
