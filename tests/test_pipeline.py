3571f0c8c2a5e51e28828ec369f5d04e2bd11ab9
ilan-gold <ilanbassgold@gmail.com>
2024-11-05 21:39:21 +0100

    (chore): add more tests, including weird zarr one

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -19,6 +19,14 @@ def chunks() -> tuple[int, ...]:
     return (2, 2)
 
 
+@pytest.fixture(params=[np.array([1, 2]), slice(1, 3)], ids=["array", "slice"])
+def indexer(request) -> slice | np.ndarray:
+    return request.param
+
+
+indexer_2 = indexer
+
+
 @pytest.fixture
 def arr(fill_value, chunks) -> zarr.Array:
     shape = (4, 4)
@@ -60,11 +68,21 @@ def test_decode(arr: zarr.Array, chunks):
     assert np.all(arr[1:3, 1:3] == np.array([[5, 6], [9, 10]]))
 
 
-def test_encode_partial(arr: zarr.Array, fill_value: int):
-    arr[1:3, 1:3] = np.array([[-1, -2], [-3, -4]])
-    print(arr[:])
+def test_encode_partial(
+    arr: zarr.Array,
+    fill_value: int,
+    indexer: slice | np.ndarray,
+    indexer_2: slice | np.ndarray,
+):
+    if isinstance(indexer, np.ndarray) and isinstance(indexer_2, np.ndarray):
+        pytest.skip(
+            "indexing across two axes with arrays seems to have strange behavior even in normal zarr"
+        )
+    stored_value = np.array([[-1, -2], [-3, -4]])
+    arr[indexer, indexer_2] = stored_value
+    res = arr[:]
     assert np.all(
-        arr[:]
+        res
         == np.array(
             [
                 [fill_value] * arr.shape[0],
@@ -72,15 +90,22 @@ def test_encode_partial(arr: zarr.Array, fill_value: int):
                 [fill_value, -3, -4, fill_value],
                 [fill_value] * arr.shape[-1],
             ]
-        )
-    )
-
-
-@pytest.mark.parametrize("indexer", [np.array([1, 2]), slice(1, 3)])
-def test_encode_singleton_axis(arr: zarr.Array, fill_value: int, indexer):
-    arr[2, indexer] = np.array([-3, -4])
+        ),
+    ), res
+    res = arr[indexer, indexer_2]
     assert np.all(
-        arr[:]
+        res == stored_value,
+    ), res
+
+
+def test_encode_singleton_axis(
+    arr: zarr.Array, fill_value: int, indexer: slice | np.ndarray
+):
+    stored_value = np.array([-3, -4])
+    arr[2, indexer] = stored_value
+    res = arr[:]
+    assert np.all(
+        res
         == np.array(
             [
                 [fill_value] * arr.shape[0],
@@ -88,5 +113,7 @@ def test_encode_singleton_axis(arr: zarr.Array, fill_value: int, indexer):
                 [fill_value, -3, -4, fill_value],
                 [fill_value] * arr.shape[0],
             ]
-        )
-    )
+        ),
+    ), res
+    res = arr[2, indexer]
+    assert np.all(res == stored_value), res
