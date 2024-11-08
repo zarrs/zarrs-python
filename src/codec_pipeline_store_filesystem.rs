zarrs/zarrs-python

commit 4c1ce390da0c5db9d8da2273f33712d69569c403

(feat): refactor `CodecPipelineStore` to a trait
- Move filesystem store implementation to a submodule

src/codec_pipeline_store_filesystem.rs
@@ -1,0 +1,36 @@
+use std::sync::Arc;
+
+use pyo3::{
+    exceptions::{PyRuntimeError, PyValueError},
+    PyErr, PyResult,
+};
+use zarrs::{filesystem::FilesystemStore, storage::ReadableWritableListableStorageTraits};
+
+use crate::{utils::PyErrExt, CodecPipelineStore};
+
+pub struct CodecPipelineStoreFilesystem {
+    store: Arc<FilesystemStore>,
+}
+
+impl CodecPipelineStoreFilesystem {
+    pub fn new() -> PyResult<Self> {
+        let store = Arc::new(FilesystemStore::new("/").map_py_err::<PyRuntimeError>()?);
+        Ok(Self { store })
+    }
+}
+
+impl CodecPipelineStore for CodecPipelineStoreFilesystem {
+    fn store(&self) -> Arc<dyn ReadableWritableListableStorageTraits> {
+        self.store.clone()
+    }
+
+    fn chunk_path<'a>(&self, store_path: &'a str) -> PyResult<&'a str> {
+        if let Some(chunk_path) = store_path.strip_prefix("file://") {
+            Ok(chunk_path.strip_prefix("/").unwrap_or(chunk_path))
+        } else {
+            Err(PyErr::new::<PyValueError, _>(format!(
+                "a filesystem store was initialised, but received a store path without a file:// prefix: {store_path}"
+            )))
+        }
+    }
+}

src/lib.rs
@@ -19,7 +19,6 @@ use zarrs::array::{
     CodecChain, DataType, FillValue,
 };
 use zarrs::array_subset::ArraySubset;
-use zarrs::filesystem::FilesystemStore;
 use zarrs::metadata::v3::array::data_type::DataTypeMetadataV3;
 use zarrs::metadata::v3::MetadataV3;
 use zarrs::storage::{ReadableWritableListableStorageTraits, StorageHandle, StoreKey};
@@ -28,58 +27,44 @@ mod utils;
 
 use utils::PyErrExt;
 
-pub enum CodecPipelineStore {
-    Filesystem(Arc<FilesystemStore>),
+mod codec_pipeline_store_filesystem;
+use codec_pipeline_store_filesystem::CodecPipelineStoreFilesystem;
+
+trait CodecPipelineStore: Send + Sync {
+    fn store(&self) -> Arc<dyn ReadableWritableListableStorageTraits>;
+    fn chunk_path<'a>(&self, store_path: &'a str) -> PyResult<&'a str>;
 }
 
+// TODO: Use a OnceLock for store with get_or_try_init when stabilised?
 #[pyclass]
 pub struct CodecPipelineImpl {
-    pub codec_chain: Arc<CodecChain>,
-    pub store: Arc<Mutex<Option<CodecPipelineStore>>>,
+    codec_chain: Arc<CodecChain>,
+    store: Mutex<Option<Arc<dyn CodecPipelineStore>>>,
     codec_options: CodecOptions,
 }
 
 impl CodecPipelineImpl {
     fn get_store_and_path<'a>(
         &self,
-        chunk_path: &'a str,
+        store_path: &'a str,
     ) -> PyResult<(Arc<dyn ReadableWritableListableStorageTraits>, &'a str)> {
         let mut gstore = self.store.lock().map_err(|_| {
             PyErr::new::<PyRuntimeError, _>("failed to lock the store mutex".to_string())
         })?;
-        if let Some(chunk_path) = chunk_path.strip_prefix("file://") {
-            if gstore.is_none() {
-                if let Some(chunk_path) = chunk_path.strip_prefix('/') {
-                    // Absolute path
-                    let store = Arc::new(FilesystemStore::new("/").map_py_err::<PyRuntimeError>()?);
-                    *gstore = Some(CodecPipelineStore::Filesystem(store.clone()));
-                    Ok((store, chunk_path))
-                } else {
-                    // Relative path
-                    let store = Arc::new(
-                        FilesystemStore::new(
-                            std::env::current_dir().map_py_err::<PyRuntimeError>()?,
-                        )
-                        .map_py_err::<PyRuntimeError>()?,
-                    );
-                    *gstore = Some(CodecPipelineStore::Filesystem(store.clone()));
-                    Ok((store, chunk_path))
-                }
-            } else if let Some(CodecPipelineStore::Filesystem(store)) = gstore.as_ref() {
-                if let Some(chunk_path) = chunk_path.strip_prefix('/') {
-                    Ok((store.clone(), chunk_path))
-                } else {
-                    Ok((store.clone(), chunk_path))
-                }
-            } else {
-                Err(PyErr::new::<PyTypeError, _>(
-                    "the store type changed".to_string(),
-                ))
+
+        #[allow(clippy::collapsible_if)]
+        if gstore.is_none() {
+            if store_path.starts_with("file://") {
+                *gstore = Some(Arc::new(CodecPipelineStoreFilesystem::new()?));
             }
-        } else {
             // TODO: Add support for more stores
+        }
+
+        if let Some(gstore) = gstore.as_ref() {
+            Ok((gstore.store(), gstore.chunk_path(store_path)?))
+        } else {
             Err(PyErr::new::<PyTypeError, _>(format!(
-                "unsupported store for {chunk_path}"
+                "unsupported store for {store_path}"
             )))
         }
     }
@@ -92,8 +77,8 @@ impl CodecPipelineImpl {
         chunk_descriptions
             .into_iter()
             .map(
-                |(chunk_path, chunk_shape, dtype, fill_value, selection, chunk_selection)| {
-                    let (store, path) = self.get_store_and_path(&chunk_path)?;
+                |(store_path, chunk_shape, dtype, fill_value, selection, chunk_selection)| {
+                    let (store, path) = self.get_store_and_path(&store_path)?;
                     let key = StoreKey::new(path).map_py_err::<PyValueError>()?;
                     Ok(ChunksItem {
                         store,
@@ -321,7 +306,7 @@ impl CodecPipelineImpl {
 }
 
 type ChunksItemRaw<'a> = (
-    // path
+    // store path
     String,
     // shape
     Vec<u64>,
@@ -371,7 +356,7 @@ impl CodecPipelineImpl {
 
         Ok(Self {
             codec_chain,
-            store: Arc::new(Mutex::new(None)),
+            store: Mutex::new(None),
             codec_options,
         })
     }

commit cb94215b72113e694c39650e685c173f95427ec0

(fix): bring back filesystem relative path support
Broken with 4c1ce39

src/codec_pipeline_store_filesystem.rs
@@ -10,12 +10,19 @@ use crate::{utils::PyErrExt, CodecPipelineStore};
 
 pub struct CodecPipelineStoreFilesystem {
     store: Arc<FilesystemStore>,
+    cwd: String,
 }
 
 impl CodecPipelineStoreFilesystem {
     pub fn new() -> PyResult<Self> {
         let store = Arc::new(FilesystemStore::new("/").map_py_err::<PyRuntimeError>()?);
-        Ok(Self { store })
+        let cwd = std::env::current_dir()?
+            .to_string_lossy()
+            .replace('\\', "/"); // TODO: Check zarr-python path handling on windows
+
+        // Remove the leading / from the cwd if preset, so cwd is a valid Zarr store path
+        let cwd = cwd.strip_prefix("/").unwrap_or(&cwd).to_string();
+        Ok(Self { store, cwd })
     }
 }
 
@@ -24,9 +31,13 @@ impl CodecPipelineStore for CodecPipelineStoreFilesystem {
         self.store.clone()
     }
 
-    fn chunk_path<'a>(&self, store_path: &'a str) -> PyResult<&'a str> {
+    fn chunk_path(&self, store_path: &str) -> PyResult<String> {
         if let Some(chunk_path) = store_path.strip_prefix("file://") {
-            Ok(chunk_path.strip_prefix("/").unwrap_or(chunk_path))
+            if let Some(chunk_path) = chunk_path.strip_prefix("/") {
+                Ok(chunk_path.to_string())
+            } else {
+                Ok(format!("{}/{}", self.cwd, chunk_path))
+            }
         } else {
             Err(PyErr::new::<PyValueError, _>(format!(
                 "a filesystem store was initialised, but received a store path without a file:// prefix: {store_path}"

src/lib.rs
@@ -32,7 +32,7 @@ use codec_pipeline_store_filesystem::CodecPipelineStoreFilesystem;
 
 trait CodecPipelineStore: Send + Sync {
     fn store(&self) -> Arc<dyn ReadableWritableListableStorageTraits>;
-    fn chunk_path<'a>(&self, store_path: &'a str) -> PyResult<&'a str>;
+    fn chunk_path(&self, store_path: &str) -> PyResult<String>;
 }
 
 // TODO: Use a OnceLock for store with get_or_try_init when stabilised?
@@ -44,10 +44,10 @@ pub struct CodecPipelineImpl {
 }
 
 impl CodecPipelineImpl {
-    fn get_store_and_path<'a>(
+    fn get_store_and_path(
         &self,
-        store_path: &'a str,
-    ) -> PyResult<(Arc<dyn ReadableWritableListableStorageTraits>, &'a str)> {
+        store_path: &str,
+    ) -> PyResult<(Arc<dyn ReadableWritableListableStorageTraits>, String)> {
         let mut gstore = self.store.lock().map_err(|_| {
             PyErr::new::<PyRuntimeError, _>("failed to lock the store mutex".to_string())
         })?;
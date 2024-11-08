cb94215b72113e694c39650e685c173f95427ec0
Lachlan Deakin <ljdgit@gmail.com>
2024-11-08 16:18:59 +1100

    (fix): bring back filesystem relative path support
    Broken with 4c1ce39

--- src/codec_pipeline_store_filesystem.rs
+++ src/codec_pipeline_store_filesystem.rs
@@ -10,12 +10,19 @@ use crate::{utils::PyErrExt, CodecPipelineStore};
 
 pub struct CodecPipelineStoreFilesystem {
     store: Arc<FilesystemStore>,
+    cwd: String,
 }
 
 impl CodecPipelineStoreFilesystem {
     pub fn new() -> PyResult<Self> {
         let store = Arc::new(FilesystemStore::new("/").map_py_err::<PyRuntimeError>()?);
-        Ok(Self { store })
+        let cwd = std::env::current_dir()?
+            .to_string_lossy()
+            .replace('\\', "/"); // TODO: Check zarr-python path handling on windows
+
+        // Remove the leading / from the cwd if preset, so cwd is a valid Zarr store path
+        let cwd = cwd.strip_prefix("/").unwrap_or(&cwd).to_string();
+        Ok(Self { store, cwd })
     }
 }
 
@@ -24,9 +31,13 @@ impl CodecPipelineStore for CodecPipelineStoreFilesystem {
         self.store.clone()
     }
 
-    fn chunk_path<'a>(&self, store_path: &'a str) -> PyResult<&'a str> {
+    fn chunk_path(&self, store_path: &str) -> PyResult<String> {
         if let Some(chunk_path) = store_path.strip_prefix("file://") {
-            Ok(chunk_path.strip_prefix("/").unwrap_or(chunk_path))
+            if let Some(chunk_path) = chunk_path.strip_prefix("/") {
+                Ok(chunk_path.to_string())
+            } else {
+                Ok(format!("{}/{}", self.cwd, chunk_path))
+            }
         } else {
             Err(PyErr::new::<PyValueError, _>(format!(
                 "a filesystem store was initialised, but received a store path without a file:// prefix: {store_path}"
