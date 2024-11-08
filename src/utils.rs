a859bd8853dcb95b2803437da29691db3c600dfe
Philipp A. <flying-sheep@web.de>
2024-11-08 13:41:25 +0100

    Simplify shape calculation

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use std::fmt::Display;
 
-use pyo3::{PyErr, PyResult, PyTypeInfo};
+use numpy::{PyUntypedArray, PyUntypedArrayMethods};
+use pyo3::{Bound, PyErr, PyResult, PyTypeInfo};
 
 pub(crate) trait PyErrExt<T> {
     fn map_py_err<PE: PyTypeInfo>(self) -> PyResult<T>;
@@ -9,5 +10,22 @@ pub(crate) trait PyErrExt<T> {
 impl<T, E: Display> PyErrExt<T> for Result<T, E> {
     fn map_py_err<PE: PyTypeInfo>(self) -> PyResult<T> {
         self.map_err(|e| PyErr::new::<PE, _>(format!("{e}")))
+    }
+}
+
+pub(crate) trait PyUntypedArrayExt {
+    fn shape_zarr(&self) -> PyResult<Vec<u64>>;
+}
+
+impl PyUntypedArrayExt for Bound<'_, PyUntypedArray> {
+    fn shape_zarr(&self) -> PyResult<Vec<u64>> {
+        Ok(if self.shape().is_empty() {
+            vec![1] // scalar value
+        } else {
+            self.shape()
+                .iter()
+                .map(|&i| u64::try_from(i))
+                .collect::<Result<_, _>>()?
+        })
     }
 }
