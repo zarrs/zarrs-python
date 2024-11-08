c945e88cdef8b2600c16ffe211b1f6baf6e6489a
ilan-gold <ilanbassgold@gmail.com>
2024-11-08 11:51:55 +0100

    (chore): xfail tests that fail on zarr-python default pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ from zarr.storage.remote import RemoteStore
 import zarrs_python  # noqa: F401
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
     from typing import Any, Literal
 
     from zarr.abc.store import Store
@@ -60,3 +61,28 @@ def array_fixture(request: pytest.FixtureRequest) -> npt.NDArray[Any]:
         .reshape(array_request.shape, order=array_request.order)
         .astype(array_request.dtype)
     )
+
+
+# tests that also fail with zarr-python's default codec pipeline
+zarr_python_default_codec_pipeline_failures = [
+    "test_roundtrip[oindex-contiguous_in_chunk_array-ellipsis]",
+    "test_roundtrip[oindex-discontinuous_in_chunk_array-ellipsis]",
+    "test_roundtrip[vindex-contiguous_in_chunk_array-ellipsis]",
+    "test_roundtrip[vindex-discontinuous_in_chunk_array-ellipsis]",
+    "test_roundtrip[oindex-across_chunks_indices_array-ellipsis]",
+    "test_roundtrip[vindex-ellipsis-across_chunks_indices_array]",
+    "test_roundtrip[vindex-across_chunks_indices_array-ellipsis]",
+    "test_roundtrip[vindex-ellipsis-contiguous_in_chunk_array]",
+    "test_roundtrip[vindex-ellipsis-discontinuous_in_chunk_array]",
+]
+
+
+def pytest_collection_modifyitems(
+    config: pytest.Config, items: Iterable[pytest.Item]
+) -> None:
+    xfail_marker = pytest.mark.xfail(
+        reason="These tests fail with the zarr-python default codec pipeline."
+    )
+    for item in items:
+        if item.name in zarr_python_default_codec_pipeline_failures:
+            item.add_marker(xfail_marker)
