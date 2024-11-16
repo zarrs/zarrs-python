d0cf8079e2c4d4a1b091bee6290273dcec93d7a4
Lachlan Deakin <ljdgit@gmail.com>
2024-11-16 11:11:32 +1100

    (fix): add `__version__`

--- python/zarrs/__init__.py
+++ python/zarrs/__init__.py
@@ -1,8 +1,10 @@
+from importlib.metadata import version
 from zarr.registry import register_pipeline
 
 from .pipeline import ZarrsCodecPipeline as _ZarrsCodecPipeline
 from .utils import CollapsedDimensionError, DiscontiguousArrayError
 
+__version__ = version("zarrs")
 
 # Need to do this redirection so people can access the pipeline as `zarrs.ZarrsCodecPipeline` instead of `zarrs.pipeline.ZarrsCodecPipeline`
 class ZarrsCodecPipeline(_ZarrsCodecPipeline):
