3901e6c7bfe5592b7dcc28e4ce0b6fd7d9b11ea0
Philipp A. <flying-sheep@web.de>
2024-11-18 09:26:11 +0100

    simplify import

--- python/zarrs/__init__.py
+++ python/zarrs/__init__.py
@@ -2,7 +2,7 @@ from zarr.registry import register_pipeline
 
 from .pipeline import ZarrsCodecPipeline as _ZarrsCodecPipeline
 from .utils import CollapsedDimensionError, DiscontiguousArrayError
-from ._internal import __version__ as __version__
+from ._internal import __version__
 
 # Need to do this redirection so people can access the pipeline as `zarrs.ZarrsCodecPipeline` instead of `zarrs.pipeline.ZarrsCodecPipeline`
 class ZarrsCodecPipeline(_ZarrsCodecPipeline):
