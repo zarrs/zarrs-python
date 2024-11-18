4ff79a33bbabfdf5a683245fb94dded4b1ce0bf1
Philipp A. <flying-sheep@web.de>
2024-11-18 12:54:43 +0100

    refactor build.rs

--- build.rs
+++ build.rs
@@ -1,16 +1,32 @@
+use std::env;
+use std::path::PathBuf;
 use std::process::Command;
-use std::{env, io};
 
-fn main() -> Result<(), String> {
-    if env::var("CARGO_BIN_NAME") == Err(env::VarError::NotPresent) {
-        // If we’re compiling the library, generate stubs first
-        if let Err(e) = Command::new("target/debug/stub_gen").spawn() {
-            if e.kind() == io::ErrorKind::NotFound {
-                eprintln!("Run `cargo build --bin stub_gen` first");
-            } else {
-                return Err(e.to_string());
-            }
-        };
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    maybe_generate_stubs()?;
     Ok(())
+}
+
+/// Generate stubs if we’re compiling the library
+/// Returns `true` if stubs were generated
+fn maybe_generate_stubs() -> Result<bool, Box<dyn std::error::Error>> {
+    if env::var("CARGO_BIN_NAME") != Err(env::VarError::NotPresent) {
+        return Ok(false);
+    }
+
+    // Find an existing `stub_gen` binary or exit silently
+    let Some(bin_path) = ["debug", "release"]
+        .into_iter()
+        .filter_map(|mode| {
+            let p = PathBuf::from(format!("target/{mode}/stub_gen"));
+            p.exists().then_some(p)
+        })
+        .next()
+    else {
+        return Ok(false);
+    };
+
+    // If we’re compiling the library, generate stubs first
+    Command::new(bin_path).spawn()?.wait()?;
+    Ok(true)
 }
