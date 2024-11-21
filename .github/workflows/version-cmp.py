81817b4c60e7123d86a00b11ff89dc7323f96197
Philipp A. <flying-sheep@web.de>
2024-11-21 11:50:50 +0100

    Add publish and release workflows (#47)
    Co-authored-by: Lachlan Deakin <ljdgit@gmail.com>

---
+++ .github/workflows/version-cmp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Can’t be an isolated script since we want to access zarrs’ metadata
+
+import importlib.metadata as im
+import os
+import sys
+from pathlib import Path
+
+from packaging.version import InvalidVersion, Version
+
+
+def set_outputs(version: Version | str) -> None:
+    is_prerelease = version.is_prerelease if isinstance(version, Version) else False
+    is_prerelease_json = "true" if is_prerelease else "false"
+    with Path(os.environ["GITHUB_OUTPUT"]).open("a") as f:
+        print(f"version={version}", file=f)
+        print(f"is_prerelease={is_prerelease_json}", file=f)
+
+
+version_tag_str = os.environ["GITHUB_REF_NAME"]
+assert version_tag_str.startswith("v"), "should be enforced in `if:` condition"
+try:
+    version_tag = Version(version_tag_str[1:])
+except InvalidVersion:
+    set_outputs("")
+    sys.exit(0)
+
+if version_tag_str[1:] != str(version_tag):
+    sys.exit(f"Tag version not normalized: {version_tag_str} should be v{version_tag}")
+
+if version_tag != (version_meta := Version(im.version("zarrs"))):
+    sys.exit(f"Version mismatch: {version_tag} (tag) != {version_meta} (metadata)")
+
+set_outputs(version_meta)
