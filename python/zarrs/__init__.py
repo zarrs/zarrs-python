64cc8cc671abc5aca38794b412aeb1d9806f6356
Kyle Barron <kyle@developmentseed.org>
2025-01-09 12:41:17 -0500

    Remove extra wrapper of ZarrsCodecPipeline

--- python/zarrs/__init__.py
+++ python/zarrs/__init__.py
@@ -1,14 +1,8 @@
 from zarr.registry import register_pipeline
 
 from ._internal import __version__
-from .pipeline import ZarrsCodecPipeline as _ZarrsCodecPipeline
+from .pipeline import ZarrsCodecPipeline as ZarrsCodecPipeline
 from .utils import CollapsedDimensionError, DiscontiguousArrayError
-
-
-# Need to do this redirection so people can access the pipeline as `zarrs.ZarrsCodecPipeline` instead of `zarrs.pipeline.ZarrsCodecPipeline`
-class ZarrsCodecPipeline(_ZarrsCodecPipeline):
-    pass
-
 
 register_pipeline(ZarrsCodecPipeline)
 
