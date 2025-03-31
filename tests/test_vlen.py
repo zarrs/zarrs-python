1837a52725f77bca84242cd0607ee801405d4849
ilan-gold <ilanbassgold@gmail.com>
2025-03-31 18:05:45 +0200

    (fix): use standard api, update test to latest version

--- tests/test_vlen.py
+++ tests/test_vlen.py
@@ -2,10 +2,10 @@ from typing import Any
 
 import numpy as np
 import pytest
-from zarr import Array
+import zarr
 from zarr.abc.codec import Codec
 from zarr.abc.store import Store
-from zarr.codecs import VLenBytesCodec, VLenUTF8Codec, ZstdCodec
+from zarr.codecs import ZstdCodec
 from zarr.core.metadata.v3 import ArrayV3Metadata, DataType
 from zarr.core.strings import _NUMPY_SUPPORTS_VLEN_STRING
 from zarr.storage import StorePath
@@ -22,27 +22,25 @@ else:
 @pytest.mark.parametrize("store", ["local"], indirect=["store"])
 @pytest.mark.parametrize("dtype", numpy_str_dtypes)
 @pytest.mark.parametrize("as_object_array", [False, True])
-@pytest.mark.parametrize(
-    "codecs", [None, [VLenUTF8Codec()], [VLenUTF8Codec(), ZstdCodec()]]
-)
+@pytest.mark.parametrize("compressor", [None, ZstdCodec()])
 def test_vlen_string(
-    *,
     store: Store,
-    dtype: None | np.dtype[Any],
+    dtype: np.dtype[Any] | None,
+    *,
     as_object_array: bool,
-    codecs: None | list[Codec],
+    compressor: Codec | None,
 ) -> None:
     strings = ["hello", "world", "this", "is", "a", "test"]
     data = np.array(strings, dtype=dtype).reshape((2, 3))
 
     sp = StorePath(store, path="string")
-    a = Array.create(
+    a = zarr.create_array(
         sp,
         shape=data.shape,
-        chunk_shape=data.shape,
+        chunks=data.shape,
         dtype=data.dtype,
         fill_value="",
-        codecs=codecs,
+        compressors=compressor,
     )
     assert isinstance(a.metadata, ArrayV3Metadata)  # needed for mypy
 
@@ -57,33 +55,31 @@ def test_vlen_string(
     assert a.dtype == expected_zarr_string_dtype
 
     # test round trip
-    b = Array.open(sp)
+    b = zarr.open(sp)
     assert isinstance(b.metadata, ArrayV3Metadata)  # needed for mypy
     assert np.array_equal(data, b[:, :])
     assert b.metadata.data_type == DataType.string
     assert a.dtype == expected_zarr_string_dtype
 
 
-@pytest.mark.parametrize("store", ["memory", "local"], indirect=["store"])
+@pytest.mark.parametrize("store", ["local"], indirect=["store"])
 @pytest.mark.parametrize("as_object_array", [False, True])
-@pytest.mark.parametrize(
-    "codecs", [None, [VLenBytesCodec()], [VLenBytesCodec(), ZstdCodec()]]
-)
+@pytest.mark.parametrize("compressor", [None, ZstdCodec()])
 def test_vlen_bytes(
-    *, store: Store, as_object_array: bool, codecs: None | list[Codec]
+    store: Store, *, as_object_array: bool, compressor: Codec | None
 ) -> None:
     bstrings = [b"hello", b"world", b"this", b"is", b"a", b"test"]
     data = np.array(bstrings).reshape((2, 3))
     assert data.dtype == "|S5"
 
     sp = StorePath(store, path="string")
-    a = Array.create(
+    a = zarr.create_array(
         sp,
         shape=data.shape,
-        chunk_shape=data.shape,
+        chunks=data.shape,
         dtype=data.dtype,
         fill_value=b"",
-        codecs=codecs,
+        compressors=compressor,
     )
     assert isinstance(a.metadata, ArrayV3Metadata)  # needed for mypy
 
@@ -97,7 +93,7 @@ def test_vlen_bytes(
     assert a.dtype == "O"
 
     # test round trip
-    b = Array.open(sp)
+    b = zarr.open(sp)
     assert isinstance(b.metadata, ArrayV3Metadata)  # needed for mypy
     assert np.array_equal(data, b[:, :])
     assert b.metadata.data_type == DataType.bytes
