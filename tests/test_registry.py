ff9470310fb3d2fb8aba31321963ba7e8429f8e6
Tom White <tom.e.white@gmail.com>
2025-09-30 16:52:43 +0100

    Add extra test suggested by @flying-sheep

---
+++ tests/test_registry.py
@@ -0,0 +1,12 @@
+import sys
+import json
+from subprocess import run
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+def test_registerable_when_not_imported():
+    proc = run([sys.executable, "-I", HERE / "data/check-registry.py"], capture_output=True, check=True)
+    results = json.loads(proc.stdout)
+    assert "zarrs" not in results["imported_modules"]
+    assert results["is_registered"]
